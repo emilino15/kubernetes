7f4ea10d5f7262db6219821ca372970b965c9759
emilino15 <160799993+emilino15@users.noreply.github.com>
2025-04-03 21:07:30 -0600

    Delete Python directory

--- Python/print_n_numbers.py
+++
@@ -1,9 +0,0 @@
-try:
-    n = 10
-    print("El número seleccionado fue: ", n)
-
-    for x in range(n):
-        print(x)
-
-except ValueError:
-    print("Error: No se puede convertir el parámetro proporcionado a entero")
